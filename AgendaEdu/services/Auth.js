3089ce46f274372b2c6f824d4bded95caa603d12
Tiago Lima <tiagolima@Tiagos-Mac-mini.local>
2019-05-13 17:22:49 -0300

    Returned to the previous code.

--- AgendaEdu/services/Auth.js
+++ AgendaEdu/services/Auth.js
@@ -1,29 +1,25 @@
 import Http from './Http'
 import Session from './Session'
 
-class Auth {
-     static signIn(email, password, callback) {
-        Http.post('/login', {
-            email,
-            password,
-        }).then((response) => {        
-            saveCredential('@Token:user', response.data)
-            callback(true, response.data, null)
-        })
-        .catch((error) => callback(false, null, error))
-    }
-    
-    static signOut(callback) {
-        Session.Credential.destroy('@Token:user')
-        .then(() => callback(true, null, null))
-        .catch((error) => callback(false, null, error))
-    }
-    
-    saveCredential(key, credential) {
-        let { token } = credential
-        Session.Credential.set(key, token)
-    }
+export default function signIn(email, password, callback) {
+    Http.post('/login', {
+        email,
+        password,
+    }).then((response) => {        
+        saveCredential('@Token:user', response.data)
+        callback(true, response.data, null)
+    })
+    .catch((error) => callback(false, null, error))
 }
 
-export default Auth;
+export default function signOut(callback) {
+    Session.Credential.destroy('@Token:user')
+    .then(() => callback(true, null, null))
+    .catch((error) => callback(false, null, error))
+}
+
+function saveCredential(key, credential) {
+    let { token } = credential
+    Session.Credential.set(key, token)
+}
 
